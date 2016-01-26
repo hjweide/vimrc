47f6f7c0348091e7dc5651434caaf7baa53b0e4a
Hendrik Weideman <weideh@rpi.edu>
2016-01-26 15:56:50 -0500

    Fixed typo.

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ if cd ~/vimrc/bundle/vim-pathogen; then git pull; else git clone https://github.
 echo "Getting Syntastic...";
 if cd ~/vimrc/bundle/syntastic; then git pull; else git clone https://github.com/scrooloose/syntastic ~/vimrc/bundle/syntastic; fi
 
-echo "Creating symbolic link for .vim and...";
+echo "Creating symbolic link for .vim and .vimrc...";
 ln -sfn ~/vimrc/ ~/.vim  # n to replace an existing symlink to directory
 ln -sf ~/vimrc/vimrcs/basic.vim ~/.vimrc;
